afc31917ba3a48137331e5c1ee02257d9af249cb
Kelly Banman <kelly.banman@gmail.com>
2012-04-02 11:50:22 -0700

    Customizable search

--- application/views/partials/search.php
+++ application/views/partials/search.php
@@ -1,3 +1,37 @@
-<form class="navbar-search" method="<?php echo $method; ?>" action="<?php echo $url; ?>">
-						<input type="text" class="search-query" placeholder="<?php echo $placeholder; ?>" name="<?php echo $name; ?>">
-					</form>
+<form class="form-search pull-right" method="<?php echo $method; ?>" action="<?php echo $url; ?>">
+							<input type="text" class="search-query" placeholder="<?php echo $placeholder; ?>">
+							<div id="search-results"><!--
+								<ul class="result-set">
+									<li class="heading"><a href="#">Users</a></li>
+									<li><a href="#">
+										<p class="title">John Brooks</p>
+										<p class="description">User since July 21, 2011</p>
+									</a></li>
+									<li><a href="#">
+										<p class="title">Jessica Beil</p>
+										<p class="description">User since August 1, 2002</p>
+									</a></li>
+								</ul>
+								<ul class="result-set">
+									<li class="heading"><a href="#">Customers</a></li>
+									<li><a href="#">
+										<p class="title">Amelia Earnhardt</p>
+										<p class="description">Last purchase yesterday</p>
+									</a></li>
+									<li><a href="#">
+										<p class="title">Amelia Earnhardt</p>
+										<p class="description">Last purchase yesterday</p>
+									</a></li>
+									<li><a href="#">
+										<p class="title">Amelia Earnhardt</p>
+										<p class="description">Last purchase yesterday</p>
+									</a></li>
+									<li><a href="#">
+										<p class="title">Amelia Earnhardt</p>
+										<p class="description">Last purchase yesterday</p>
+									</a></li>
+								</ul>
+								<ul class="result-set">
+                </ul> -->
+							</div>
+						</form>
--- application/views/template.php
+++ application/views/template.php
@@ -49,43 +49,7 @@
 						<ul class="nav">
 							<?php echo Section::yield('nav'); ?>
 						</ul>
-						<form class="form-search pull-right">
-							<input type="text" class="search-query" placeholder="Search for">
-							<div id="search-results"><!--
-								<ul class="result-set">
-									<li class="heading"><a href="#">Users</a></li>
-									<li><a href="#">
-										<p class="title">John Brooks</p>
-										<p class="description">User since July 21, 2011</p>
-									</a></li>
-									<li><a href="#">
-										<p class="title">Jessica Beil</p>
-										<p class="description">User since August 1, 2002</p>
-									</a></li>
-								</ul>
-								<ul class="result-set">
-									<li class="heading"><a href="#">Customers</a></li>
-									<li><a href="#">
-										<p class="title">Amelia Earnhardt</p>
-										<p class="description">Last purchase yesterday</p>
-									</a></li>
-									<li><a href="#">
-										<p class="title">Amelia Earnhardt</p>
-										<p class="description">Last purchase yesterday</p>
-									</a></li>
-									<li><a href="#">
-										<p class="title">Amelia Earnhardt</p>
-										<p class="description">Last purchase yesterday</p>
-									</a></li>
-									<li><a href="#">
-										<p class="title">Amelia Earnhardt</p>
-										<p class="description">Last purchase yesterday</p>
-									</a></li>
-								</ul>
-								<ul class="result-set">
-                </ul> -->
-							</div>
-						</form>
+						<?php echo $search; ?>
 					</div><!--/.nav-collapse -->
 				</div>
 			</div>
