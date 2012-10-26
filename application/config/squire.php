8a372e0e572e1b5c1dd3cb6655fb56f7c05bbfa0
Kelly Banman <kelly.banman@gmail.com>
2012-10-25 19:26:57 -0700

    Change default squire config

--- application/config/squire.php
+++ application/config/squire.php
@@ -2,9 +2,32 @@
 
 return array(
 
-	'partials' => array(
-		//'template' => 'layout.template',
-		'top' => 'layout.top',
-	),
+	/*
+	|--------------------------------------------------------------------------
+	| Site Name 
+	|--------------------------------------------------------------------------
+	|
+	| HTML that constitutes the site logo. It is wrapped in a link to the
+	| app homepage.
+	|
+	*/
 
+	'site_name' => '<span style="color:#fff">Squire</span>App',
+
+
+	/*
+	|--------------------------------------------------------------------------
+	| Page Title
+	|--------------------------------------------------------------------------
+	|
+	| Template for the page title text. It is run through sprintf and passed
+	| the page_title variable.
+	| 
+	| This format can be overriden by explicitly defining the 'title' variable
+	| on the template or changing via runtime configuration.
+	|
+	*/
+
+	'title' => "%s | SquireApp",
+	
 );
